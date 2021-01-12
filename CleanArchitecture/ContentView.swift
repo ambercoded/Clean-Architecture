c64ed94a02aac179a6b9edffcc3f718eaa7456e2
Adrian <mailforadrian@me.com>
2021-01-12 09:33:40 +0100

    Initial Commit

---
+++ CleanArchitecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CleanArchitecture
+//
+//  Created by Adrian on 12.01.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
